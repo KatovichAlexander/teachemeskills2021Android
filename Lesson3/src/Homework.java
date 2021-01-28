7ba1e8b47bcedf2df9718d538cfeb1ff861d8227
KatovichAlexander <katovich.a@gmail.com>
2021-01-28 18:23:14 +0300

    Sorry, it's so late and not completely. I will definitely finish it ...

---
+++ Lesson3/src/Homework.java
@@ -0,0 +1,150 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+        System.out.println(operation(1));
+//        System.out.println(operation(0));
+        foobar(6);
+//        foobar(10);
+//        foobar(15);
+        printMatrix();
+        printPrimeNumbers();
+    }
+
+    /**
+     * Method should perform some operation with int "number" based on some conditions:
+     * - if number is positive - increase it by 1
+     * - if number is negative - decrease it by 2
+     * - if number is equal to zero - replace it with 10
+     * return number value after operation
+     */
+    public static int operation(int number) {
+        int a = 0;
+        if (a > 0) {
+            a++;
+        }
+        if (a < 0) {
+            a = a - 2;
+        }
+        if (a == 0) {
+            a = 10;
+        }
+        return a;
+    }
+
+    /**
+     * Method should print different strings in console based on some conditions:
+     * - if remainder of the division number by 3 is zero - print "foo" (example of number - 6)
+     * - if remainder of the division number by 5 is zero - print "bar" (example of number - 10)
+     * - if remainder of the division number by both 3 and 5 is zero 0  and - print "foobar" (example of number - 15)
+     */
+
+    public static void foobar(int number) {
+        int a = 15;
+        int b;
+        int d;
+
+        b = a % 3;
+        d = a % 5;
+
+        if (b == 0 & d != 0) {
+            System.out.println("foo");
+//            if (d == 0) {
+//                System.out.println("bar");
+//            }
+        }
+
+        if (d == 0 & b != 0) {
+            System.out.println("bar");
+        }
+
+        if (b == 0 & d == 0) {
+            System.out.println("foobar");
+        }
+    }
+
+    /**
+     * Steps to implement:
+     * - Read two ints from console
+     * - Create a two-dimensional int array (use ints that you read from console height and width)
+     * - Fill array with random values (under 100)
+     * - Print in console matrix with given size, but:
+     * - If remainder of the division array element by 3 is zero - print "+" sign instead of array element value
+     * - If remainder of the division array element by 7 is zero - print "-" sign instead of array element value
+     * - Print "*" otherwise
+     * <p>
+     * Example:
+     * - Values from console -  2 and 3
+     * - Array will look like this (values be different because it's a random)
+     * 6 11 123
+     * 1 14 21
+     * - For this values output in console should be:
+     * <p>
+     * + * *
+     * * - +
+     * <p>
+     * Note that 21 % 3 == 0 and 21 % 7 = 0, but output is not +-, but +
+     */
+
+    public static void printMatrix() {
+
+//        - Read two ints from console
+//        int height = 2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input height: ");
+        int height = scanner.nextInt();
+
+//        int width = 2;
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Input width: ");
+        int width = scanner1.nextInt();
+
+//        System.out.println( a );
+//        System.out.println( b );
+
+//        - Create a two-dimensional int array (use ints that you read from console height and width)
+//        int[][] twoDArray = new int[i][j];
+        int[][] twoDArray = new int[height][width];
+
+//        - Fill array with random values (under 100)
+        Random random = new Random();
+        for (int[] row : twoDArray) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] = random.nextInt(100);
+            }
+        }
+
+//        - Print in console matrix with given size:
+        for (int i = 0; i < twoDArray.length; i++) {
+            for (int j = 0; j < twoDArray[i].length; j++) {
+                System.out.print(twoDArray[i][j] + " ");
+            }
+        }
+
+//        - If remainder of the division array element by 3 is zero - print "+" sign instead of array element value
+
+
+//        int[][] ArraySymbol = new int[height][width];
+//
+//        for (int i = 0; i < twoDArray.length; i++) {
+//            if (twoDArray[i]%3 == 0) {
+//                System.out.print(twoDArray[i] + " ");
+//            for (int j = 0; j < twoDArray[i].length; j++) {
+//                System.out.print(twoDArray[i][j] + " ");
+//            }
+//        }
+
+
+    }
+
+    /**
+     * (optional)
+     * Method should print all prime numbers < 1000
+     */
+    public static void printPrimeNumbers() {
+
+    }
+
+
+}
